9a2544c0b3a0f6cd23f5d5b8e96c1d64b9d899c2
Chiwen.L <zxcvbnm840508@gmail.com>
2022-09-05 23:09:59 +0900

    hide TabBar in ChatView

--- app/Controls.js
+++ app/Controls.js
@@ -42,8 +42,16 @@ const ChatStackScreen = () => {
   );
 };
 
-///  Main Control ///
+// Hide TabBar in ChatView //
+const getRouteName = (route) => {
+  const routeName = getFocusedRouteNameFromRoute(route);
+  if (routeName?.includes('ChatView')) {
+    return 'none';
+  }
+  return 'absolute';
+};
 
+///  Main Control ///
 export default function MainControl() {
   const colorScheme = useColorScheme();
 
@@ -65,7 +73,18 @@ export default function MainControl() {
       <Tab.Screen
         name='Chat'
         component={ChatStackScreen}
-        options={{
+        options={({ route }) => ({
+          tabBarStyle: {
+            display: getRouteName(route),
+            width: '80%',
+            position: 'absolute',
+            marginBottom: '10%',
+            borderRadius: 50,
+            overflow: 'hidden',
+            left: 40,
+            paddingBottom: 0,
+            justifyContent: 'space-around',
+          },
           headerShown: false,
           tabBarIcon: ({ focused }) =>
             !focused ? (
@@ -81,7 +100,7 @@ export default function MainControl() {
                 color={colorScheme === 'dark' ? 'white' : 'black'}
               />
             ),
-        }}
+        })}
       />
       <Tab.Screen
         name='Calls'
